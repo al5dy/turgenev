b8018f56157aa4470286fa5228809916d4602d1d
Anton <ant.lokotkov@gmail.com>
2020-07-19 22:21:44 +0300

    Add build production flags & update build.bat file

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 SET version=1.0
 SET plugin=turgenev
+
+call npm run build
+
 for %%a in ("build" "includes" "languages") do (xcopy /s /d /i /y "%%~a" ".\%plugin%\%%~a" )
 for %%a in (".\%plugin%.php" ".\readme.txt") do (xcopy /d /i /y "%%~a" ".\%plugin%")
 7z a -tzip -mx9 %plugin%.%version%.zip %plugin%
@@ -8,6 +11,8 @@ del %plugin%.%version%.zip
 
 :: Wordpress SVN
 ::SLEEP 1
+@RD /S /Q ".\wordpress\trunk"
+@RD /S /Q ".\wordpress\assets"
 for %%a in ("build" "includes" "languages") do (xcopy /s /d /i /y "%%~a" ".\wordpress\trunk\%%~a" )
 for %%a in (".\%plugin%.php" ".\readme.txt") do (xcopy /d /i /y "%%~a" ".\wordpress\trunk")
 xcopy /s /y .\src\assets\*.* .\wordpress\assets\
